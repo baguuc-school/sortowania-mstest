ec7dc1aac30311450ae5e5b64434c7ca26c9ee1a
Jan Tadeusz Żukowski <4886712@teb-gdansk.ad>
2026-02-11 13:24:46 +0100

    util: cleanup in Program.cs

--- Sortowania/Program.cs
+++ Sortowania/Program.cs
@@ -5,9 +5,6 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        InsertSortStrategy<int> strategy = new InsertSortStrategy<int>();
-        int[] result = strategy.Sort(new int[] { 5, 3, 3, 1, 2, 4 });
-
-        Console.WriteLine(string.Join(", ", result));
+        Console.WriteLine("Sortowanie...");
     }
 }
